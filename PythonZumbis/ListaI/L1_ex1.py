596b8d5037818e898d6864764ce7753bf3dd5d7e
lidiane monteiro <LidyMonteiro@MacBook-de-Lidiane-Monteiro.local>
2015-04-27 16:54:03 -0300

    Exercicios Python para Zumbis

---
+++ PythonZumbis/ListaI/L1_ex1.py
@@ -0,0 +1,4 @@
+# 1)Faça um programa que peça dois números inteiros e imprima a soma desses dois números
+n1 = int(input('Digite um número inteiro: '))
+n2 = int(input('Digite um número inteiro: '))
+print ('Resultado: %d' %(n1+n2))
---
+++ PythonZumbis/ListaI/L1_ex2.py
@@ -0,0 +1,3 @@
+# 2)Escreva um programa que leia um valor em metros e o exiba convertido em milímetros
+m = float(input('Digite a distância em metros: '))
+print ('Milimetro: ', m*1000)
