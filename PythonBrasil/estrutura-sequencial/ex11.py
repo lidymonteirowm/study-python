3697ac45bcca988f2377f230bfd59c9447801cac
Lidy Monteiro <lidianedesouzamonteiro@gmail.com>
2015-04-29 01:41:20 -0300

    ex11.py

---
+++ PythonBrasil/estrutura-sequencial/ex11.py
@@ -0,0 +1,16 @@
+''' 
+11) Faça um programa que peça 2 números inteiros e um número real. Calcule e mostre: 
+- O produto do dobro do primeiro com metade do segundo .
+- A soma do triplo do primeiro com o terceiro.
+- O terceiro elevado ao cubo.
+'''
+n1 = int(input('1o Número inteiro: '))
+n2 = int(input('2o Número inteiro: '))
+n3 = float(input('Número real: '))
+
+print ('Soma:', ((2*n1) * (n2/2)))
+print ('Produto:', (3 * n1) + n3)
+print ('Cubo:', n3**3)
+
+
+
