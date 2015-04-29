dd02343467a539f3d82342522e9b0dc25582e12e
Lidy Monteiro <lidianedesouzamonteiro@gmail.com>
2015-04-29 01:42:22 -0300

    ex12.py

---
+++ PythonBrasil/estrutura-sequencial/ex12.py
@@ -0,0 +1,5 @@
+''' 12) Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule 
+seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58 '''
+
+altura = float(input('Altura:'))
+print ('Peso ideal:', (72.7*altura) - 58)
