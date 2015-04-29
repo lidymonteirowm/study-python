76dbd769e567fd4d3bc4d070c8b5a35fae64fbcb
Lidy Monteiro <lidianedesouzamonteiro@gmail.com>
2015-04-29 11:34:15 -0300

    Create ex18.py

---
+++ PythonBrasil/estrutura-sequencial/ex18.py
@@ -0,0 +1,6 @@
+''' 18) Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet (em Mbps), 
+calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos). '''
+
+tamanho = float(input('Tamanho do arquivo (MB): '))
+velocidade = float(input('Velocidade de Internet (MBps): '))
+print('Tempo aproximado de download: %.0f Minutos ' %((tamanho / velocidade) * 60))
